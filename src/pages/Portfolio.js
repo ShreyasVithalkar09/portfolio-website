11ca61f84225cdc6a2853c7c5e0fbf8fe8cb1a08
ShreyasVithalkar09 <shreyas.vithalkar08@gmail.com>
2022-06-17 12:25:53 +0530

    some changes

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Card from "../components/Card";
 import Heading from "../components/Heading";
-import { projects } from "../data";
+import { projects } from "./data";
 
 
 const Portfolio = () => {
---
+++ src/pages/data.js
@@ -0,0 +1,38 @@
+
+export const projects =[
+        
+        {
+            "title":"Text Utils",
+            "content":"TextUtils - A utility to manipulate the text as you want in the browser!. Users can manipulate the text like Copy text, Word Count, Character Count, Remove Extra Spaces.",
+            "stack": ["React JS", "Bootstrap"],
+            "poster": "/assets/text-utils.png",
+            "link": "https://shreyasvithalkar09.github.io/TextUtils-App/",
+            "id":1
+        },
+        {
+            "title":"Newzii - A news app",
+            "content":"This is a news app built with React JS using news-API which allows us to read top headlines and detailed news of different categories.",
+            "stack": ["React JS", "Bootstrap"],
+            "poster": "./assets/newzii.png",
+            "link": "https://github.com/ShreyasVithalkar09/Newzii",
+            "id":2
+        },
+        {
+            "title":"SGPA Calculator",
+            "content":"This is a SGPA Calculator for calculating the SGPA of VTU students of 5th Semester. Student can enter all subject marks out of 100 and get their SGPA and Percentage.",
+            "stack": ["HTML", "Bulma CSS", "Javascript"],
+            "poster": "./assets/sgpa.png",
+            "link": "https://shreyasvithalkar09.github.io/sgpa-calculator.github.io/",
+            "id":3
+        },
+        {
+            "title":"Tic-Tac-Toe",
+            "content":"This is a Tic-Tac-Toe game which we all used to play in our Childhood. As a part of my learning I've build this small project under the Instructor Mr.Hitesh Choudhary Sir.",
+            "stack": ["React JS", "Bootstrap"],
+            "poster": "./assets/tic-tac-toe.png",
+            "link": "https://shreyasvithalkar09.github.io/tic-tac-toe-react/",
+            "id":4        }
+    ]
+    
+
+
