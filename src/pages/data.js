2a81622daa4091a8c907ab8514ede0f604005354
ShreyasVithalkar09 <shreyas.vithalkar08@gmail.com>
2024-11-18 21:08:16 +0530

    Added new project

--- src/pages/data.js
+++ src/pages/data.js
@@ -1,12 +1,36 @@
 export const projects = [
   {
+    title: "Shopping Cart Web Application",
+    content:
+      "This is a shopping cart web application built with ReactJS, created as a hobby project to explore modern web technologies, improve my ReactJS skills, and implement features commonly found in e-commerce platforms.",
+    stack: [
+      "React JS",
+      "Stripe Payment",
+      "Express JS",
+      "Clerk Auth",
+      "Daisy UI",
+    ],
+    poster: "/assets/shopping-cart.png",
+    link: "https://github.com/ShreyasVithalkar09/react-stripe-ecomm",
+    id: 1,
+  },
+  {
+    title: "Blogapp React Appwrite",
+    content:
+      "This is a Full-Stack Blog app built with ReactJS and Appwrite. As a part of my learning I've build this project under the Instructor Mr.Hitesh Choudhary Sir. On top of that added my design and some other features",
+    stack: ["React JS", "Appwrite", "Tailwind CSS", "React-Hook-Form"],
+    poster: "./assets/blog.png",
+    link: "https://github.com/ShreyasVithalkar09/blog-appwrite-react",
+    id: 2,
+  },
+  {
     title: "Text Utils",
     content:
       "TextUtils - A utility to manipulate the text as you want in the browser!. Users can manipulate the text like Copy text, Word Count, Character Count, Remove Extra Spaces.",
     stack: ["React JS", "Bootstrap"],
     poster: "/assets/text-utils.png",
     link: "https://shreyasvithalkar09.github.io/TextUtils-App/",
-    id: 1,
+    id: 3,
   },
   {
     title: "Newzii - A news app",
@@ -15,16 +39,7 @@ export const projects = [
     stack: ["React JS", "Bootstrap"],
     poster: "./assets/newzii.png",
     link: "https://github.com/ShreyasVithalkar09/Newzii",
-    id: 2,
-  },
-  {
-    title: "SGPA Calculator",
-    content:
-      "This is a SGPA Calculator for calculating the SGPA of VTU students of 5th Semester. Student can enter all subject marks out of 100 and get their SGPA and Percentage.",
-    stack: ["HTML", "Bulma CSS", "Javascript"],
-    poster: "./assets/sgpa.png",
-    link: "https://shreyasvithalkar09.github.io/sgpa-calculator.github.io/",
-    id: 3,
+    id: 4,
   },
   {
     title: "Tic-Tac-Toe",
@@ -33,15 +48,6 @@ export const projects = [
     stack: ["React JS", "Bootstrap"],
     poster: "./assets/tic-tac-toe.png",
     link: "https://shreyasvithalkar09.github.io/tic-tac-toe-react/",
-    id: 4,
-  },
-  {
-    title: "Blogapp React Appwrite",
-    content:
-      "This is a Full-Stack Blog app built with ReactJS and Appwrite. As a part of my learning I've build this project under the Instructor Mr.Hitesh Choudhary Sir. On top of that added my design and some other features",
-    stack: ["React JS", "Appwrite", "Tailwind CSS", "React-Hook-Form"],
-    poster: "./assets/blog.png",
-    link: "https://reactjs-blogapp.netlify.app/",
     id: 5,
   },
 ];
